d4d9a81b99b0348f2e5557eba246248ca334c8fc
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2026-03-22 00:46:10 +0100

    fix: update safelock repo

--- Casks/safelock.rb
+++ Casks/safelock.rb
@@ -2,10 +2,10 @@ cask "safelock" do
   version "2.2.0"
   sha256 "caea0447a4de162dd356566eeac2748837a025a1f424606d82ee53f40548e191"
 
-  url "https://github.com/Zarox28/SafeLock/releases/download/v#{version}/SafeLock.dmg"
+  url "https://github.com/Zarox28-Projects/SafeLock/releases/download/v#{version}/SafeLock.dmg"
   name "SafeLock"
   desc "Simple MacOS app that locks the screen and records with the webcam if any movement is detected"
-  homepage "https://github.com/Zarox28/SafeLock"
+  homepage "https://github.com/Zarox28-Projects/SafeLock"
 
   app "SafeLock.app"
 
