7a9c7f633d0e6258c7f95f46f93fd3f5d5625566
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2026-03-22 00:44:12 +0100

    fix: update activate repo

--- Casks/activate.rb
+++ Casks/activate.rb
@@ -2,10 +2,10 @@ cask "activate" do
   version "1.2.0"
   sha256 "192e0ed47b9d6b78088ffc5d731764a6929dce78434fc4d23faee9992b27b087"
 
-  url "https://github.com/Zarox28/Activate/releases/download/v#{version}/Activate.dmg"
+  url "https://github.com/Zarox28-Projects/Activate/releases/download/v#{version}/Activate.dmg"
   name "Activate"
   desc "Activate MacOS is a playful macOS app that mimics the Windows activation message"
-  homepage "https://github.com/Zarox28/Activate"
+  homepage "https://github.com/Zarox28-Projects/Activate"
 
   app "Activate.app"
 
